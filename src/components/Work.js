9b80bd03675318a63edce45eb7b98358487fb9dc
mmms2000 <moseskmcmmms@gmail.com>
2025-11-04 20:30:22 +0900

    workCard fix the grid

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,7 +7,7 @@ import { NavLink } from "react-router-dom";
 const Work = () => {
   return (
     <div className="work-container">
-      <h1 className="project-heading"></h1>
+      <h1 className="project-heading"> Projects</h1>
       <div className="project-container">
         {WorkCardData.map((val,index)=>{
           return (
--- src/components/Workcard.js
+++ src/components/Workcard.js
@@ -3,9 +3,6 @@ import { NavLink } from "react-router-dom"
 
 const Workcard =(props) => {
   return (
-    <div className="work-container"> 
-    <h1 className="project-heading"></h1>
-    <div className="project-container">
         <div className="project-card">
             <img src={props.imgsrc} alt="image" />
             <h2 className="project-title"> {props.title}</h2>
@@ -18,8 +15,6 @@ const Workcard =(props) => {
                 </div>
             </div>
         </div>
-    </div>
-    </div>
   )
 }
 
